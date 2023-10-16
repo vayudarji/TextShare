52d7c9780bc431f53ba9a78a196e7e3f50659225
vayu darji <vayutailor@gmail.com>
2023-10-16 19:19:06 +0530

    reading data

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
-import { getFirestore , addDoc , serverTimestamp} from "firebase/firestore"
+import { getFirestore , addDoc , serverTimestamp , collection , query , orderBy , limit , getDocs} from "firebase/firestore"
 import { app } from "../firebase"
 
 const firestore = getFirestore(app);
 
-const writeData = async () => {
+const Share = () => {
   const [message, setMessage] = useState("")
+
+const writeData = async () => {
   const  result = await addDoc(collection(firestore,"user"),{
     code: code,
     message: message,
@@ -15,15 +17,48 @@ console.log("result",result);
 
 }
 
-const Share = () => {
+const getDocumentByQuery = async () => {
+  const collectionRef = collection(firestore , "user");
+  const q = query( collectionRef , orderBy('timestamp','desc'), limit(1) );
+  const getSnapshot = await getDocs( q );
+  getSnapshot.forEach((doc) => {
+      const data = doc.data();
+      console.log('Last document data:', data);
+      console.log(data.message);
+      
+      if (encode === data.code) {
+          setText(`${data.message}`)
+          
+      } else {
+          console.log("wrong");
+      }
+      console.log(data.code)
+      
+      
+  });
+  
+  
+};
+
+
+
   return (
     <div>
           <label>enter text here</label>
           <input type="text" placeholder='text here' onChange={(e)=>setMessage(e.target.value)} value={message} />
           <button onClick={writeData} >add data</button>
           <br />
+          <label>enter code here</label>
+            <input type="text" placeholder='text here' onChange={(e)=>setCode(e.target.value)} value={code} />
+            <br />
+            <label>enter en code here</label>
+            <input type="text" placeholder='text here' onChange={(e)=>setEncode(e.target.value)} value={encode} />
+        <button onClick={getDocumentByQuery}>read</button>
+        <h1>Text</h1>
+        <p>{text}</p>
     </div>
   )
+
 }
 
 export default Share
