6f869d675eda60a9600cca144611fec4f07ee769
vayu darji <vayutailor@gmail.com>
2023-10-17 01:55:13 +0530

    fixing

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { getFirestore , addDoc , serverTimestamp , collection , query , orderBy , limit , getDocs} from "firebase/firestore"
 import { app } from "../firebase"
+import "./Share.css";
 
 const firestore = getFirestore(app);
 
@@ -16,7 +17,7 @@ const writeData = async () => {
     message: message,
     timestamp: serverTimestamp(),
 });
-console.log("result",result);
+
 
 }
 
@@ -26,8 +27,7 @@ const getDocumentByQuery = async () => {
   const getSnapshot = await getDocs( q );
   getSnapshot.forEach((doc) => {
       const data = doc.data();
-      console.log('Last document data:', data);
-      console.log(data.message);
+      
       
       if (encode === data.code) {
           setText(`${data.message}`)
@@ -35,25 +35,35 @@ const getDocumentByQuery = async () => {
       } else {
           console.log("wrong");
       }
-      console.log(data.code)
+      
       
   });
 };
 
   return (
-    <div>
-          <label>Enter text here</label>
-          <input type="text" placeholder='Text' onChange={(e)=>setMessage(e.target.value)} value={message} />
+    <div className='container' >
+      <div className='top-text'>
+          <label className='text-1' >Enter text here</label>
+          <div >            
+          <input className='input-container' type="text" onChange={(e)=>setMessage(e.target.value)} value={message} />
+          </div>
+      </div>
+      <div className='text-2'>
+          <label className='text-1' >Code (optional)</label>
           <br />
-          <label>Enter code (optional)</label>
-          <input type="text" placeholder='Code' onChange={(e)=>setCode(e.target.value)} value={code} />
+          <input className='' type="text" placeholder='Code' onChange={(e)=>setCode(e.target.value)} value={code} />
           <br />
-          <button onClick={writeData} >Share Text</button>
+          <div className=''>
+          <button className='' onClick={writeData} >Share Text</button>
+          </div>
+      </div>
           <br />
-            <label>Enter encryption code here</label>
-            <input type="text" placeholder='Encryption code' onChange={(e)=>setEncode(e.target.value)} value={encode} />
+      <div className='part-3 div' >
+            <label className='text-1' >Enter decryption code here</label>
+            <input type="text" placeholder='Decryption code' onChange={(e)=>setEncode(e.target.value)} value={encode} />
         <button onClick={getDocumentByQuery}>Get Text</button>
         <h1>Text</h1>
+      </div>
         <p>{text}</p>
     </div>
   )
